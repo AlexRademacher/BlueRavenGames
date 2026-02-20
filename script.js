76b922f92bce0967f6fe90f70df86f8571f6b44e
AlexRademacher <178829078+AlexRademacher@users.noreply.github.com>
2026-02-20 01:20:41 -0500

    Main menu fixes and start of Projects
    Features:
    - Updated information on the site
    - Added image for The Wyrm
    - Fixed the main menu to work better and look better
    - got tabs working on Projects

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function buttonClicked() {
     document.getElementById("mainDiv").textContent =
         "Button Clicked times: " + numButtonClicks;
 }
+
+function testTabs(evt, tabNum) {
+    var i, tabContent, tabs;
+    var alreadyPressed = false;
+
+    
+    if (document.getElementById(tabNum).style.display == "block") {
+        alreadyPressed = true;
+    }
+    
+    // remove all other info
+    tabContent = document.getElementsByClassName("TabContent");
+    for (i = 0; i < tabContent.length; i++){
+        tabContent[i].style.display = "none";
+    }
+    
+    // remove active state from all tab buttons
+    tabs = document.getElementsByClassName("Tab");
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].className = tabs[i].className.replace(" active", "");
+    }
+    
+    if (!alreadyPressed){
+        // reveal the info of the tab pressed and set it active
+        document.getElementById(tabNum).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+}
